52ae478ae21829cc24a1f5da62e4cb2ea620b0f3
girma <girmatarekegn.gi@gmail.com>
2022-11-07 14:13:43 +0300

    Add script.js from last week project and update to es6

--- modules/script.js
+++ modules/script.js
@@ -2,21 +2,21 @@ import Book from './bookClass.js';
 
 export default class UI {
     static getBookLS = () => {
-        let bookLocalStore;
-        if (localStorage.getItem('books') === null) {
-          bookLocalStore = [];
-        } else {
-          bookLocalStore = JSON.parse(localStorage.getItem('books'));
-        }
-        return bookLocalStore;
+      let bookLocalStore;
+      if (localStorage.getItem('books') === null) {
+        bookLocalStore = [];
+      } else {
+        bookLocalStore = JSON.parse(localStorage.getItem('books'));
       }
-    
+      return bookLocalStore;
+    }
+
       static add = (book) => {
         const bookList = this.getBookLS();
         bookList.push(book);
         localStorage.setItem('books', JSON.stringify(bookList));
       }
-    
+
       static remove = (title, author) => {
         const bookList = this.getBookLS();
         bookList.forEach((book, index) => {
@@ -26,16 +26,16 @@ export default class UI {
         });
         localStorage.setItem('books', JSON.stringify(bookList));
       }
-    
+
       static displayBooks = () => {
         const books = this.getBookLS();
         books.forEach((book) => this.libraryBooks(book));
       }
-    
+
       static libraryBooks = (book) => {
         const library = document.querySelector('.library');
         const newLib = document.createElement('div');
-    
+
         newLib.innerHTML = `<div class="new-lib ${book.author}">
           <p><q>${book.title}</q> by <i>${book.author}</i></p>
             <button class="remove">
@@ -45,22 +45,22 @@ export default class UI {
         newLib.className = 'color-block';
         library.appendChild(newLib);
       }
-    
+
       static deleteBook = (el) => {
         if (el.classList.contains('remove')) {
           el.parentElement.remove();
         }
       }
-    
+
       static clearFields = () => {
         document.querySelector('.input-book').value = '';
         document.querySelector('.input-author').value = '';
       }
-    
+
       static createBooks = (title, author) => {
         const book = new Book(title, author);
-         this.libraryBooks(book);
-         this.add(book);
-         this.clearFields();
+        this.libraryBooks(book);
+        this.add(book);
+        this.clearFields();
       }
 }
